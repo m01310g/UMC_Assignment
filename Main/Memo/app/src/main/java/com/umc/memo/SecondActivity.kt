f399977c163c4f99649dae0b89da774e7030dfde
m01310g <m01310g@gmail.com>
2024-05-07 14:12:34 +0900

    Week5 Lifecycle Practice - Memo Application

---
+++ Main/Memo/app/src/main/java/com/umc/memo/SecondActivity.kt
@@ -0,0 +1,21 @@
+package com.umc.memo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.umc.memo.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.secondTv.text = intent.getStringExtra("data")
+
+    }
+}
